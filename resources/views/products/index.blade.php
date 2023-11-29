baef53f91c5d6977beed0ca205ce358b7126667e
IvanHuang <d1104181014@gm.lhu.edu.tw>
2023-11-29 11:15:06 +0800

    新增表格內的顯示、修改、刪除

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,38 +7,14 @@
 <body>
 <h1>列出所有商品</h1>
 
-<table>
-    <tr>
-        <th>商品名</th>
-        <th>價格</th>
-        <th>優惠</th>
-        <th>庫存量</th>
-        <th>連鎖便利店</th>
-        <th>操作1</th>
-        <th>操作2</th>
-        <th>操作3</th>
-    </tr>
+@for($i=0; $i<count($products); $i++)
+    {{$products[$i]['name'] }} <br/>
+    {{$products[$i]['price'] }} <br/>
+    {{$products[$i]['discount'] }} <br/>
+    {{$products[$i]['tid'] }} <br/>
+    {{$products[$i]['inventory'] }} <br/></br>
 
-
-
-
-    @for($i=0; $i<count($products); $i++)
-        <tr>
-            <td>{{ $products[$i]['name'] }} </td>
-            <td>{{ $products[$i]['price'] }} </td>
-            <td>{{ $products[$i]['discount'] }} </td>
-            <td>{{ $products[$i]['tid'] }} </td>
-            <td>{{ $products[$i]['inventory'] }} </td>
-            <td><a href="{{ route('products.show',['id'=>$products[$i]['id']]) }}" > 顯示</a></td>
-            <td><a href="{{ route('products.edit',['id'=>$products[$i]['id']]) }}" > 修改</a></td>
-            <td>刪除</td>
-        </tr>
-
-
-    @endfor
-
-<table>
+@endfor
 
 </body>
 </html>
-
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,16 @@ Route::get('/', function() {
     return redirect('products');
 });
 
+// 顯示顯示所有商品資料
 Route::get('products', [ProductsController::class, 'index'])->name('products.index');
+// 顯示單一商品資料
+Route::get('players/{id}', [ProductsController::class, 'show'])->where('id', '[0-9]+')->name('products.show');
+// 修改單一商品表單
+Route::get('players/{id}/edit', [ProductsController::class, 'edit'])->where('id', '[0-9]+')->name('products.edit');
 
-Route::get('stores', [StoresController::class, 'index'])->name('stores.index');
+// 顯示顯示所有商店資料
+Route::get('teams', [StoresController::class, 'index'])->name('stores.index');
+// 顯示單一商店資料
+Route::get('teams/{id}', [StoresController::class, 'show'])->where('id', '[0-9]+')->name('stores.show');
+// 修改單一商店表單
+Route::get('teams/{id}/edit', [StoresController::class, 'edit'])->where('id', '[0-9]+')->name('stores.edit');
