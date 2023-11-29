0a0753be76f487c3ab8b40f4fe5105083ed18d62
vivi_lin <d1104181026@gm.lhu.edu.tw>
2023-11-29 10:17:58 +0800

    新增表格中的顯示，修改，刪除

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,24 +14,30 @@
         <th>優惠</th>
         <th>庫存量</th>
         <th>連鎖便利店</th>
-        <th>建立時間</th>
-        <th>修改時間</th>
+        <th>操作1</th>
+        <th>操作2</th>
+        <th>操作3</th>
     </tr>
 
 
 
 
-@for($i=0; $i<count($products); $i++)
-    <tr>
-       <td>{{$products[$i]['name'] }} </td>
-       <td>{{$products[$i]['price'] }} </td>
-       <td>{{$products[$i]['discount'] }} </td>
-       <td>{{$products[$i]['tid'] }} </td>
-       <td>{{$products[$i]['inventory'] }} </td>
-       <td></td>
+    @for($i=0; $i<count($products); $i++)
+        <tr>
+            <td>{{ $products[$i]['name'] }} </td>
+            <td>{{ $products[$i]['price'] }} </td>
+            <td>{{ $products[$i]['discount'] }} </td>
+            <td>{{ $products[$i]['tid'] }} </td>
+            <td>{{ $products[$i]['inventory'] }} </td>
+            <td><a href="{{ route('products.show',['id'=>$products[$i]['id']]) }}" > 顯示</a></td>
+            <td><a href="{{ route('products.edit',['id'=>$products[$i]['id']]) }}" > 修改</a></td>
+            <td>刪除</td>
+        </tr>
 
 
-@endfor
+    @endfor
+
+<table>
 
 </body>
 </html>
