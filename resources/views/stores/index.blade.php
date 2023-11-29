5faa4ad113d931eb153ab2d16124719bad578179
Xian20030225 <s0983540896@gmail.com>
2023-11-29 13:22:42 +0800

    增加版模配置 app, footer, header, 並修改 players.index, 及 teams.index 等視圖

---
+++ resources/views/stores/index.blade.php
@@ -0,0 +1,32 @@
+
+@extends('app')
+
+@section('title', '便利店網站 - 列出所有商店')
+
+@section('nba_contents')
+<h1>列出所有商店</h1>
+
+<table>
+    <tr>
+        <th>編號</th>
+        <th>連鎖便利店</th>
+        <th>官網</th>
+        <th>建立時間</th>
+        <th>修改時間</th>
+        <th>操作1</th>
+        <th>操作2</th>
+        <th>操作3</th>
+    </tr>
+    @for($i=0; $i<count($stores); $i++)
+        <tr>
+            <td>{{ $stores[$i]['id'] }}</td>
+            <td>{{ $stores[$i]['inventory'] }}</td>
+            <td>{{ $stores[$i]['official web'] }}</td>
+            <td><a href="{{ route('stores.show', ['id'=>$stores[$i]['id']]) }}">顯示</a></td>
+            <td><a href="{{ route('stores.edit', ['id'=>$stores[$i]['id']]) }}">修改</a></td>    
+            <td>刪除</td>    
+        </tr>
+    @endfor
+<table>
+
+@endsection
