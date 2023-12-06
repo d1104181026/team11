fed3809359f029521bc23b2072f8fe789b4ade37
YiXuan_Lin <xuannn1127@gmail.com>
2023-12-06 14:23:58 +0800

    新增routes/web

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,20 @@ use App\Http\Controllers\StoresController;
 Route::get('/', function() {
     return redirect('products');
 });
-
+//顯示所有商品資料
 Route::get('products', [ProductsController::class, 'index'])->name('products.index');
+//顯示單一商品資料
+Route::get('products/{id}', [ProductsController::class, 'show'])->where('id', '[0-9]+')->name('products.show');
+//修改單一商品資料
+Route::get('products/{id}/edit', [ProductsController::class, 'edit'])->where('id', '[0-9]+')->name('products.edit');
+//刪除單一商品資料
+Route::delete('products/delete/{id}', [ProductsController::class, 'destroy'])->where('id', '[0-9]+')->name('products.destroy');
 
+//顯示所有商店資料
 Route::get('stores', [StoresController::class, 'index'])->name('stores.index');
+//顯示單一商店資料
+Route::get('stores/{id}', [StoresController::class, 'show'])->where('id', '[0-9]+')->name('stores.show');
+//修改單一商店資料
+Route::get('stores/{id}/edit', [StoresController::class, 'edit'])->where('id', '[0-9]+')->name('stores.edit');
+//刪除單一商店及所屬商品資料
+Route::delete('stores/delete/{id}', [StoresController::class, 'destroy'])->where('id', '[0-9]+')->name('stores.destroy');
