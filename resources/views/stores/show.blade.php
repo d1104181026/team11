5dd9fe780bfa7dc682aeba54c6f2a8cd92047033
YiXuan_Lin <xuannn1127@gmail.com>
2023-12-06 13:32:41 +0800

    在商店頁面下方顯示有販售的商品

--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -6,4 +6,30 @@
 編號：{{ $product->id }}<br/>
 連鎖便利店：{{ $product->inventory }}<br/>
 官網：{{ $product->official web }}<br/>
+
+<h1>{{ $product->inventory }}的所有商品</h1>
+
+<table>
+    <tr>
+        <th>商品名</th>
+        <th>價格</th>
+        <th>優惠</th>
+        <th>庫存量</th>
+        <th>連鎖便利店</th>
+    </tr>
+
+
+
+
+    @foreach ($players as $player)
+        <tr>
+            <td>{{ $product->name }} </td>
+            <td>{{ $product->price }} </td>
+            <td>{{ $product->discount }} </td>
+            <td>{{ $product->store->name }} </td>
+            <td>{{ $product->inventory }} </td>
+        </tr>
+    @endforeach
+
+<table>
 @endsection
