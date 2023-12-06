c4964a50e22bcb1141c94b333d9c4f0e8437fe50
YiXuan_Lin <xuannn1127@gmail.com>
2023-12-06 14:24:26 +0800

    Debug

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,6 +8,6 @@
 商品名：{{ $product->name }}<br/>
 價格：{{ $product->price }}<br/>
 優惠：{{ $product->discount }}<br/>
-庫存量：{{ $product->store->name }}<br/>
-連鎖便利店：{{ $product->inventorye }}<br/>
+庫存量：{{ $product->inventory }}<br/>
+連鎖便利店：{{ $product->store->name }}<br/>
 @endsection
