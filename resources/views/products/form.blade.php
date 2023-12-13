128058cfeb15485354754e7203b1f9efccacf2f7
vivi_lin <d1104181026@gm.lhu.edu.tw>
2023-12-13 10:46:35 +0800

    修改商品介面表格

--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -1,30 +1,27 @@
 <div class="form-group">
-    {!! Form::label('name', '商品姓名：') !!}
+    {!! Form::label('name', '商品名稱：') !!}
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('tid', '所屬商店：') !!}
     <!-- 待設計一下拉式選單 -->
 </div>
+
 <div class="form-group">
-    {!! Form::label('birthdate','名稱:') !!}
-    {!! Form::date('birthdate',null ,['class'=>'form-control']) !!}
+    {!! Form::label('price','價格:') !!}
+    {!! Form::text('price',null ,['class'=>'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('onboarddate','價格:') !!}
-    {!! Form::date('onboarddate',null ,['class'=>'form-control']) !!}
+    {!! Form::label('discount', '優惠：') !!}
+    {!! Form::text('discount', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('position', '優惠：') !!}
-    {!! Form::text('position', null, ['class' => 'form-control']) !!}
+    {!! Form::label('tid', '連鎖便利店：') !!}
+    {!! Form::text('tid', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('height', '連鎖便利店：') !!}
-    {!! Form::text('height', null, ['class' => 'form-control']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('weight', '庫存量：') !!}
-    {!! Form::text('weight', null, ['class' => 'form-control']) !!}
+    {!! Form::label('inventory', '庫存量：') !!}
+    {!! Form::text('inventory', null, ['class' => 'form-control']) !!}
 </div>
 
 <div class="form-group">
