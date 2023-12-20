eab303d06f27637e6d50c1c252ceafb31e113032
YiXuan_Lin <xuannn1127@gmail.com>
2023-12-20 10:39:02 +0800

    為products.edit視圖增加資料驗證的錯誤訊息

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -5,6 +5,7 @@
 @section('product_theme','編輯中的商品')
 
 @section('product_contents')
+    @include('message.list')
     {!! From::model($product , ['method' => 'PATCH', 'action' => ['\App\Http\Controllers\ProductsController@update',$product->id]]) !!}
     @include('products.from',['submitButtonText'=>'更新商品資料'])
     {!! From::close() !!}
