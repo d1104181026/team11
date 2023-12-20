8981f09cf4d796d151b591b7c2f35b10ba14aa19
vivi_lin <d1104181026@gm.lhu.edu.tw>
2023-12-20 10:37:00 +0800

    設計 CreateProductRequest 類別，用以驗證表單欄位的資料正確性；2. 增加 message.list 視圖，用以顯示資料驗證的結果。

---
+++ app/Http/Requests/CreateProductRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:2|max:191',
+            'id' => 'required',
+            'tid' => 'required',
+            'price' => 'nullable',
+            'discount' => 'nullable',
+            'inventory' => 'required|string|min:2|max:191',
+            
+            ];
+    }
+    public function messages()
+    {
+        return [
+            "name.required" => "商品名稱 為必填",
+            "name.min" => "商品名稱 至少需2個字元",
+            "id.required" => "商品名稱 為必填",
+            "tid.required" => "商店名稱 為必填",
+            "price" => "商品價格 為必填",
+            "discount" => "商品優惠 為必填",
+            "inventory" => "商品庫存量 必須為數字",
+            
+        ];
+    }    
+}
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,6 +5,7 @@
 @section('players_theme', '建立商品的表單')
 
 @section('nba_contents')
+    @include('message.list')
     {!! Form::open(['url' => 'products/store']) !!}
     @include('products.form', ['submitButtonText'=>"新增商品資料"])
     {!! Form::close() !!}
