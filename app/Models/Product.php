1f1328df7f41e671f0dee06195b11da484a3430f
vivi_lin <d1104181026@gm.lhu.edu.tw>
2023-12-27 12:39:09 +0800

    add return to model's scope functions

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,5 +15,17 @@ class Product extends Model
         'tid',
         'inventory'
     ];
+
+    public function stores()
+
+    {
+        return $this->belongsTo('App\Models\Store','tid','id');
+    }
+
+    public function scopeSenior($query)
+    {
+        
+        return $query->where('price', '>', 10)->orderBy('price', 'asc');
+    }
     
 }
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -13,4 +13,23 @@ class Store extends Model
         'name',
         'web'
     ];
+
+    public function products()
+
+    {
+        return $this->belongsTo('App\Models\Store','tid');
+    }
+
+    public function delete()
+    {
+        $this->products()->delete();
+        return parent::delete();
+    }       
+
+    public function scopeZone($query, $zone)
+    {
+        
+        return $query->where('zone', '=', $zone);
+    }
+
 }
